5d39988fc67f31aa45df8c30965b38f222214a8c
harshiniyelgam <yelgamharshini@gmail.com>
2026-06-12 11:22:50 +0530

    todo app final

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Task
 
 def addTask(request):
@@ -8,3 +8,33 @@ def addTask(request):
         return redirect('/')
 
     return redirect('/')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('/') 
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('/')  
+def edit_task(request,pk):
+    get_task=get_object_or_404(Task, pk=pk)
+    if request.method=='POST':
+        new_task=request.POST['task']
+        get_task.task=new_task
+        get_task.save()
+        return redirect('/')
+    else:
+        context={
+            'get_task':get_task,        
+        }    
+        return render(request,'edit_task.html',context)    
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('/')
+
+
